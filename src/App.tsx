0f95b8a9c33930838ca43098af8dd1fdabcb3830
Paul Peter Callaghan <calgahan@live.co.uk>
2024-10-19 01:16:47 +0100

    Got rid of welcome tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -35,13 +35,6 @@ function App() {
             {todo.content}</li>
         ))}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
       <button onClick={signOut}>Sign out</button>
     </main>
   );
